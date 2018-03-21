363eb200eaf42aa5c9e5b0f7f13610abeb464cba
serii <seriiburduja@gmail.com>
2018-03-21 11:43:57 +0200

    add styles and scripts

--- wp-content/themes/theme1/header.php
+++ wp-content/themes/theme1/header.php
@@ -2,56 +2,36 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <!-- <link rel="stylesheet" type="text/css" href="style.css" /> -->
 
-  <!-- <script src="js/jquery-1.10.1.min.js" type="text/javascript"></script>
-  <script src="js/jqFancyTransitions.1.8.min.js" type="text/javascript"></script> -->
-
-<title><?php wp_get_document_title();
-	bloginfo( "name" ); ?></title>
-	
+<title><?php bloginfo( 'name' );
+	wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
 
 <body>
-
 <div class="head-wrapper">
-  <div class="head">
-      <div class="head-logo">
-        <a href="/"><img src="<?php bloginfo( 'template_url' ); ?>/images/logo.jpg" alt=""/></a>
+	<div class="head">
+    	<div class="head-logo">
+        <a href="<?php echo home_url();?>"><img src="<?php bloginfo( 'template_url' ) ?>/images/logo.jpg" alt=""/></a>
       </div>
-        <div class="head-banner">
-          <!-- <?php $banner = new WP_Query( [ 'post_type' => 'banner', 'posts_per_page' => 1, 'order' => 'ASC' ] ); ?>
-           -->
-	
-	        <?php if ( $banner->have_posts() ) : while ( $banner->have_posts() ) : $banner->the_post(); ?>
-		        <?php the_post_thumbnail( 'full' ); ?>
-	        <?php endwhile; ?>
-              <!-- post navigation -->
-	        <?php else: ?>
-              <h2>Баннер из виджетов</h2>
-	        <?php endif; ?>
-
-          <!-- <img src="<?php bloginfo( "template_url" ); ?>/images/img1.jpg" alt='' />
-                        <img src="<?php bloginfo( "template_url" ); ?>/images/img1.jpg" alt='' />
-                        <img src="<?php bloginfo( "template_url" ); ?>/images/img1.jpg" alt='' />             -->
-        </div>
+        <div class="head-banner"><img src="<?php bloginfo( 'template_url' ) ?>/images/728x90.jpg" alt=""/></div>
     </div>
 </div>
-
-
 <div class="menu-wrapper">
-  <div class="menu-main">
-
-      <?php if ( ! dynamic_sidebar( "header-menu" ) ): ?>
-        <span>Это место для меню</span>
-      <?php endif; ?>
-
-    <ul class="ico-social">
-        <li><a href="#"><img src="<?php bloginfo( "template_url" ); ?>/images/ico-vk.png" alt="мы вконтакте"/></a></li>
-          <li><a href="#"><img src="<?php bloginfo( "template_url" ); ?>/images/ico-youtobe.png" alt=""/></a></li>
-          <li><a href="#"><img src="<?php bloginfo( "template_url" ); ?>/images/ico-facebook.png" alt=""/></a></li>
-          <li><a href="#"><img src="<?php bloginfo( "template_url" ); ?>/images/ico-twitter.png" alt=""/></a></li>
+	<div class="menu-main">
+    	<ul class="menu">
+        	<li><a href="#">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Design Services</a></li>
+            <li><a href="#">Request Quote</a></li>
+            <li><a href="#">Advertise</a></li>
+            <li><a href="#">Contact Me</a></li>
+        </ul>
+        <ul class="ico-social">
+        	<li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/images/ico-vk.png" alt="мы вконтакте"/></a></li>
+            <li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/images/ico-youtobe.png" alt="канал youtobe"/></a></li>
+            <li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/images/ico-facebook.png" alt="мы на facebook"/></a></li>
+            <li><a href="#"><img src="<?php bloginfo( 'template_url' ) ?>/images/ico-twitter.png" alt="наш twitter"/></a></li>
         </ul>
     </div>
 </div>
--- wp-content/themes/theme1/index.php
+++ wp-content/themes/theme1/index.php
@@ -1,61 +1,88 @@
-<?php get_header(); ?>
+<?php get_header();?>
 <div class="content-wrapper">
 	<div class="content-main">
-    	<div class="content">
+    	
+        <div class="content">
+        
+            <div id='slideshowHolder'>
+    
+             <img src="<?php bloginfo('template_url') ?>/images/img1.jpg" alt='' />
+            
+             <img src="<?php bloginfo('template_url') ?>/images/img1.jpg" alt='' />
+            
+             <img src="<?php bloginfo('template_url') ?>/images/img1.jpg" alt='' />
+            
+            </div>
+     
+        	<div class="articles">
+            	
+                <div class="articles-gen-img">
+                	<a href="#"><img src="<?php bloginfo('template_url') ?>/images/post-img1.jpg" alt="Preview image" /></a>
+            	</div>
+                <div class="articles-head">
+            		<span class="articles-date"><img src="<?php bloginfo('template_url') ?>/images/articles-autor.jpg" alt="admin" /> <span>Admin</span> - Nov 28th, 2010</span>
+                    <span class="articles-comments"><img src="<?php bloginfo('template_url') ?>/images/articles-comment.jpg" alt="commets" /> <a href="#">10 комментариев</a></span>
+            	</div>
+                
+                <h1><a href="#">Thanksgiving greeting card PSD</a></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus...</p>
 
-               <div id='slideshowHolder'>
-                    <?php $slider = new WP_Query( [ 'post_type' => 'slider', 'order' => 'ASC' ] ); ?>
-	
-	               <?php if ( $slider->have_posts() ) : while ( $slider->have_posts() ) : $slider->the_post(); ?>
-		               <?php the_post_thumbnail(); ?>
-	               <?php endwhile; ?>
-	
-	               <?php else: ?>
-                     <h2>Слайдер из виджетов</h2>
-	               <?php endif; ?>
-
-                 <!-- <img src="<?php bloginfo( "template_url" ); ?>/images/img1.jpg" alt='' />
-                        <img src="<?php bloginfo( "template_url" ); ?>/images/img2.jpg" alt='' />
-                        <img src="<?php bloginfo( "template_url" ); ?>/images/img3.jpg" alt='' /> -->
-                </div>
-	  
-	      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+				<p><a href="#">Read More</a></p>
+            
+            </div>
+            
             <div class="articles">
+            	
+                <div class="articles-gen-img">
+                	<a href="#"><img src="<?php bloginfo('template_url') ?>/images/post-img2.jpg" alt="Preview image" /></a>
+            	</div>
+                <div class="articles-head">
+            		<span class="articles-date"><img src="<?php bloginfo('template_url') ?>/images/articles-autor.jpg" alt="admin" /> <span>Admin</span> - Nov 28th, 2010</span>
+                    <span class="articles-comments"><img src="<?php bloginfo('template_url') ?>/images/articles-comment.jpg" alt="commets" /> <a href="#">10 комментариев</a></span>
+            	</div>
+                
+                <h1><a href="#">Thanksgiving greeting card PSD</a></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus...</p>
 
-                    <div class="articles-gen-img">
-                        <a href="<?php the_permalink(); ?>">
-                            <?php if ( has_post_thumbnail() ): ?>
-	                            <?php the_post_thumbnail(); ?>
-                            <?php else: ?>
-                              <img src="<?php bloginfo( "template_url" ); ?>/images/no-image.jpg" alt=''/>
-                            <?php endif; ?>
-                        </a>
-                    </div>
+				<p><a href="#">Read More</a></p>
+            
+            </div>
+            
+            <div class="articles">
+            	
+                <div class="articles-gen-img">
+                	<a href="#"><img src="<?php bloginfo('template_url') ?>/images/post-img3.jpg" alt="Preview image" /></a>
+            	</div>
+                <div class="articles-head">
+            		<span class="articles-date"><img src="<?php bloginfo('template_url') ?>/images/articles-autor.jpg" alt="admin" /> <span>Admin</span> - Nov 28th, 2010</span>
+                    <span class="articles-comments"><img src="<?php bloginfo('template_url') ?>/images/articles-comment.jpg" alt="commets" /> <a href="#">10 комментариев</a></span>
+            	</div>
+                
+                <h1><a href="#">Thanksgiving greeting card PSD</a></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus...</p>
 
-                    <div class="articles-head">
-                        <span class="articles-date">
-                            <img src="<?php bloginfo( "template_url" ); ?>/images/articles-autor.jpg" alt=""/>
-                            <span><?php the_author(); ?></span> - <?php the_time( 'M d, Y' ); ?> <!-- Nov 28th, 2010 -->
-                        </span>
-                        <span class="articles-comments"><img src="<?php bloginfo( "template_url" ); ?>/images/articles-comment.jpg" alt=""/> <a href="#">10 Comments</a></span>
-                    </div>
+				<p><a href="#">Read More</a></p>
+            
+            </div>
+            
+            <div class="articles">
+            	
+                <div class="articles-gen-img">
+                	<a href="#"><img src="<?php bloginfo('template_url') ?>/images/post-img4.jpg" alt="Preview image" /></a>
+            	</div>
+                <div class="articles-head">
+            		<span class="articles-date"><img src="<?php bloginfo('template_url') ?>/images/articles-autor.jpg" alt="admin" /> <span>Admin</span> - Nov 28th, 2010</span>
+                    <span class="articles-comments"><img src="<?php bloginfo('template_url') ?>/images/articles-comment.jpg" alt="commets" /> <a href="#">10 комментариев</a></span>
+            	</div>
+                
+                <h1><a href="#">Thanksgiving greeting card PSD</a></h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum malesuada orci a commodo. Aenean dapibus urna quis nulla consequat sagittis. Quisque ut ultrices massa. Mauris felis felis, rutrum sit amet vehicula ut, tempus quis lectus...</p>
 
-                    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-			
-			    <?php the_excerpt(); ?>
-
-              <p><a class="read-more" href="<?php the_permalink(); ?>">Read More</a></p>
-                </div>
-	  
-	      <?php endwhile; ?>
-            <!-- post navigation -->
-	      <?php else: ?>
-            <!-- no posts found -->
-	      <?php endif; ?>
-
-
-        <div class="pager">
+				<p><a href="#">Read More</a></p>
+            
+            </div>
+            
+            <div class="pager">
             	<a href="#">1</a>
                 <a href="#">2</a>
                 <a href="#">3</a>
@@ -64,9 +91,98 @@
                 <a href="#">75</a>
                 <a href="#" class="prev-next">Next</a>
             </div>
+        
+        </div>
+        
+        <div class="sidebar-main">
+        	
+            <div class="sidebar-widget">
+            	<div class="search-main">
+                    <form action="" id="search-form" method="get">
+                        
+                        <input class="search-txt" type="text" value="Поиск" onBlur="if(this.value=='')this.value='Поиск'" onFocus="if(this.value=='Поиск')this.value=''" />
+                        <input type="image" src="<?php bloginfo('template_url') ?>/images/search-btn.jpg" />
+                        
+                    </form>
+                </div>
+            </div>
+            
+            <div class="sidebar-widget">
+            	<ul class="ads-main">
+                	<li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/ads1.jpg" alt="" /></a></li>
+                    <li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/ads2.jpg" alt="" /></a></li>
+                    <li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/ads3.jpg" alt="" /></a></li>
+                    <li><a href="#"><img src="<?php bloginfo('template_url') ?>/images/ads4.jpg" alt="" /></a></li>
+                </ul>
+            </div>
+            
+            <div class="sidebar-widget">
+            	<h3>Категории</h3>
+                
+                <ul class="side-categories">
+                	<li><a href="#">Backgrounds</a></li>
+                    <li><a href="#">Brushes</a></li>
+                    <li><a href="#">Design Articles</a></li>
+                    <li><a href="#">Fonts</a></li>
+                    <li><a href="#">Free PSD Files</a></li>
+                    <li><a href="#">Icons</a></li>
+                    <li><a href="#">Logos</a></li>
+                    <li><a href="#">Patterns</a></li>
+                    <li><a href="#">Textures</a></li>
+                    <li><a href="#">Tools & Utilities</a></li>
+                </ul>
+                <ul class="side-categories">
+                	<li><a href="#">Backgrounds</a></li>
+                    <li><a href="#">Brushes</a></li>
+                    <li><a href="#">Design Articles</a></li>
+                    <li><a href="#">Fonts</a></li>
+                    <li><a href="#">Free PSD Files</a></li>
+                    <li><a href="#">Icons</a></li>
+                    <li><a href="#">Logos</a></li>
+                    <li><a href="#">Patterns</a></li>
+                    <li><a href="#">Textures</a></li>
+                    <li><a href="#">Tools & Utilities</a></li>
+                </ul>
+                
+            </div>
+            
+            <div class="sidebar-widget">
+            	<h3>Последние записи</h3>
+                <p class="latest-post">
+                	<img src="<?php bloginfo('template_url') ?>/images/latest-past1.jpg" alt="" />
+                    <a href="#">Thanksgiving greeting card PSD</a>
+                </p>
+                <p class="latest-post">
+                	<img src="<?php bloginfo('template_url') ?>/images/latest-past1.jpg" alt="" />
+                    <a href="#">Thanksgiving greeting card PSD</a>
+                </p>
+                <p class="latest-post">
+                	<img src="<?php bloginfo('template_url') ?>/images/latest-past1.jpg" alt="" />
+                    <a href="#">Thanksgiving greeting card PSD</a>
+                </p>
+                <p class="latest-post">
+                	<img src="<?php bloginfo('template_url') ?>/images/latest-past1.jpg" alt="" />
+                    <a href="#">Thanksgiving greeting card PSD</a>
+                </p>
+                <p class="latest-post">
+                	<img src="<?php bloginfo('template_url') ?>/images/latest-past1.jpg" alt="" />
+                    <a href="#">Thanksgiving greeting card PSD</a>
+                </p>
+                <p class="latest-post">
+                	<img src="<?php bloginfo('template_url') ?>/images/latest-past1.jpg" alt="" />
+                    <a href="#">Thanksgiving greeting card PSD</a>
+                </p>
+                
+            </div>
+            
+            <div class="sidebar-widget">
+            	<div class="facebook">
+                	<img src="<?php bloginfo('template_url') ?>/images/facebook.jpg" alt="" />
+                </div>
+            </div>
             
         </div>
-	  
-	  <?php get_sidebar(); ?>
-	  
-	  <?php get_footer(); ?>
+    </div>
+    
+</div>
+<?php get_footer();?>
